9b4ba6bb52d57c0aa6a5239cd6fc54e2bd8085be
Boniface Pereira <bonifacepereira@gmail.com>
2017-03-02 18:42:41 +0530

    fix

--- src/org/apache/cordova/plugin/SignalStrengthDualSim.java
+++ src/org/apache/cordova/plugin/SignalStrengthDualSim.java
@@ -3,6 +3,7 @@ package org.apache.cordova.plugin;
 import android.content.Context;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
+import android.telephony.MultiSimTelephonyManager;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
