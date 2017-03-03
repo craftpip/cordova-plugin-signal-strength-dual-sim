f5a537243bacf30fc743c13ae753dc0700ee3a11
Boniface Pereira <bonifacepereira@gmail.com>
2017-03-03 20:08:49 +0530

    fix

--- www/signal-strength-dual-sim.js
+++ www/signal-strength-dual-sim.js
@@ -1,14 +1,14 @@
 function SignalStrengthDualSim() {
-    this.sim = function (callback) {
+    this.sim1 = function (callback) {
         return cordova.exec(callback, function (err) {
             callback(err);
         }, "SignalStrengthDualSim", 'Sim1', []);
     };
-    // this.sim2 = function (callback) {
-    //     return cordova.exec(callback, function (err) {
-    //         callback(err);
-    //     }, "SignalStrengthDualSim", 'Sim2', []);
-    // };
+    this.sim2 = function (callback) {
+        return cordova.exec(callback, function (err) {
+            callback(err);
+        }, "SignalStrengthDualSim", 'Sim2', []);
+    };
     // this.simCount = function (callback) {
     //     return cordova.exec(callback, function (err) {
     //         callback(err);
