28afa0293a34ae51daea4e9327613b1845a0e745
Boniface Pereira <bonifacepereira@gmail.com>
2017-03-03 16:44:31 +0530

    fix

--- src/org/apache/cordova/plugin/SignalStrengthDualSim.java
+++ src/org/apache/cordova/plugin/SignalStrengthDualSim.java
@@ -69,15 +69,15 @@ public class SignalStrengthDualSim extends CordovaPlugin {
                 slot = 1;
             }
 
-            SubscriptionInfo subscriptionInfoObject = mSubscriptionManager.getActiveSubscriptionInfoForSimSlotIndex(slot);
-
-            if (subscriptionInfoObject == null) {
-                this.callback.error("Subscription returned null");
-            }
-
-            int subId = subscriptionInfoObject.getSubscriptionId();
-            LOG.i(LOG_TAG, "SubID: " + subId);
-
+//            SubscriptionInfo subscriptionInfoObject = mSubscriptionManager.getActiveSubscriptionInfoForSimSlotIndex(slot);
+//
+//            if (subscriptionInfoObject == null) {
+//                this.callback.error("Subscription returned null");
+//            }
+//
+//            int subId = subscriptionInfoObject.getSubscriptionId();
+//            LOG.i(LOG_TAG, "SubID: " + subId);
+//
             ssListener = new SignalStrengthStateListener();
 //            TelephonyManager defaultTelephonyManager  = (TelephonyManager) cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
             Context context = cordova.getActivity().getApplicationContext();
@@ -86,9 +86,9 @@ public class SignalStrengthDualSim extends CordovaPlugin {
 
             TelephonyManager telephonyManagerSub;
             try {
-                telephonyManagerSub = (TelephonyManager) defaultTelephonyManager.createForSubscriptionId(1);
-                LOG.i(LOG_TAG, "2: " + subId);
-                telephonyManagerSub.listen(ssListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
+//                telephonyManagerSub = (TelephonyManager) defaultTelephonyManager.createForSubscriptionId(1);
+//                LOG.i(LOG_TAG, "2: " + subId);
+                defaultTelephonyManager.listenGemini(ssListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS, slot);
                 LOG.i(LOG_TAG, "3: " + subId);
             } catch (Exception e) {
                 LOG.i(LOG_TAG, "2ERR: " + e.getMessage());
